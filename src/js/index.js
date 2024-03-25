ef93e732253b1c7475c5f5d4dd08b4726549cf27
vaanskii <giorgivanadze03@gmail.com>
2024-03-25 19:03:46 +0400

    Integrate box in three.js - Integrate box and axesHelper. - Integrate orbitcontrols where user can move box rotate and zoom.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,37 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerHeight, window.innerWidth);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+); 
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.set(0, 2, 5);
+orbit.update();
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box)
+
+
+
+function animate(time) {
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate)
