400dffe1ae9584b3ec0ef6921319e5d38e00ee11
vaanskii <giorgivanadze03@gmail.com>
2024-03-25 21:59:17 +0400

    Integrate sphere. - Integrate sphere. - Integrate options where user can change sphere color, speed and make wireframe true or false.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -7,7 +8,7 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-    75,
+    45,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
@@ -18,7 +19,7 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.set(0, 2, 5);
+camera.position.set(-10, 30, 30);
 orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
@@ -26,11 +27,55 @@ const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box)
 
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
 
+
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0000FF,
+    wireframe: false
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+
+sphere.position.set(0, 4, 0)
+
+const gui = new dat.GUI();
+const options = {
+    sphereColor: '#ffea00',
+    wireframe: false,
+    speed: 0.01
+};
+
+gui.addColor(options, 'sphereColor').onChange(function(e) {
+    sphere.material.color.set(e);
+});
+
+gui.add(options, 'wireframe').onChange(function(e){
+    sphere.material.wireframe = e;
+})
+
+gui.add(options, 'speed', 0, 0.1);
+
+let step = 0;
 
 function animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
+
+    step += options.speed
+    sphere.position.y = 10 * Math.abs(Math.sin(step));
+
     renderer.render(scene, camera);
 }
 
